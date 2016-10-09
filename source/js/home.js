456edb8a3a7e21433f2a51229a93aecc42a8e828
iTimeTraveler <xuewenlong.2008@gmail.com>
2016-10-09 10:36:05 +0800

    fix navigation bar when scroll in homepage.

--- source/js/home.js
+++ source/js/home.js
@@ -1,4 +1,18 @@
-function insert(){
-   var insertText = "<table><tr><td>any thing</td></tr></table>";
-   document.getElementById("insert").innerHTML(insertText);
+var navbar = document.getElementsByClassName("intro-navigate")[0],
+window.addEventListener("scroll", scrollHandler);
+
+function getStyle(obj, attri) {
+ 	return obj.currentStyle ? obj.currentStyle[attri] : window.getComputedStyle(obj, null)[attri];
+}
+
+function scrollHandler(e) {
+	 var event = e || window.event,
+	     target = event.target || event.srcElement;
+	 var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+	 var navbarY = (scrollTop / 1.2);
+	 if (scrollTop > h - navbar.offsetHeight) {
+	     navbar.classList.add("fixed");
+	 } else {
+	     navbar.classList.remove("fixed");
+	 }
 }
