70b7bb99f6150e8c266c7ef01066e87a70e47438
iTimeTraveler <xuewenlong.2008@gmail.com>
2016-10-13 15:20:13 +0800

    add back to top function.

--- source/js/home.js
+++ source/js/home.js
@@ -17,17 +17,3 @@ function scrollHandler(e) {
 	     navbar.classList.remove("fixed");
 	 }
 }
-
-
-$(window).scroll(function() {
-    $(window).scrollTop() > 500 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
-});
-$("#rocket").click(function() {
-    $("#rocket").addClass("launch");
-    $("html, body").animate({
-        scrollTop: 0
-    }, 500, function() {
-        $("#rocket").removeClass("show launch");
-    });
-    return false;
-});
