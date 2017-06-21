f98bb951b763b760425541351011602af54ce5d8
iTimeTraveler <xuewenlong.2008@gmail.com>
2017-06-21 15:45:21 +0800

    standardize import postion of javascript files.

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -15,6 +15,16 @@
     }, searchAnimDuration);
   };
 
+  var s = [
+        '<div style="display: none;">',
+          '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
+        '</div>'
+      ].join('');
+
+  var di = $(s);
+
+  $('#container').append(di);
+
   $('#nav-search-btn').on('click', function(){
     if (isSearchAnim) return;
 
